d91a91239a93de417d943acda8c969c5375b76b2
Rico Putra Pradana <pradanaricoputra@gmail.com>
2022-08-28 16:25:03 +0700

    +T1 set buttons in Hero section responsive

--- components/atoms/Buttons.js
+++ components/atoms/Buttons.js
@@ -1,7 +1,21 @@
 import { Button } from "@chakra-ui/react";
 import styled from "@emotion/styled";
 
-export const ButtonPrimary = styled(Button)`
+const ButtonResponsive = styled(Button)`
+  font-size: 14px;
+  font-weight: normal;
+  padding: 24px 42px;
+
+  @media only screen and (max-width: 424px) {
+    padding: 16px 24px;
+  }
+
+  @media only screen and (max-width: 320px) {
+    padding: 12px 16px;
+  }
+`;
+
+export const ButtonPrimary = styled(ButtonResponsive)`
   color: var(--chakra-colors-white);
   font-size: 14px;
   font-weight: normal;
@@ -9,7 +23,7 @@ export const ButtonPrimary = styled(Button)`
   background: var(--chakra-colors-blue-primary);
 `;
 
-export const ButtonSecondary = styled(Button)`
+export const ButtonSecondary = styled(ButtonResponsive)`
   color: var(--chakra-colors-blue-primary);
   font-size: 14px;
   font-weight: normal;
@@ -18,7 +32,7 @@ export const ButtonSecondary = styled(Button)`
   border: 1px solid var(--chakra-colors-blue-primary);
 `;
 
-export const ButtonTertiary = styled(Button)`
+export const ButtonTertiary = styled(ButtonResponsive)`
   color: var(--chakra-colors-blue-primary);
   font-size: 14px;
   font-weight: normal;
